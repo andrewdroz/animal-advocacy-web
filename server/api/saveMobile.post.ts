97e02407d8bb456946fe23e7900cc875556f9b65
andrewdroz <andrewdroz@gmail.com>
2025-07-13 13:40:26 +0800

    root level await

--- server/api/saveMobile.post.ts
+++ server/api/saveMobile.post.ts
@@ -1,8 +1,7 @@
 import { JSONFilePreset } from "lowdb/node";
 
-const db = await JSONFilePreset<Array<string>>('db.json', [])
-
 export default defineEventHandler<{ body: { number: string } }>(async (event) => {
+    const db = await JSONFilePreset<Array<string>>('db.json', [])
     const body = await readBody(event);
     await db.update(( mobileNumbers ) => mobileNumbers.push(body.number));
 });
